b52f93f629247886ba2dc81938209284e7f6df59
mamadjavaad <mamadjavad8237@gmail.com>
2023-09-16 18:22:06 +0330

    Adding vuetify ➕

--- src/main.js
+++ src/main.js
@@ -1,11 +1,17 @@
 import './assets/base.css'
-
+import 'vuetify/styles'
 import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
-
+import { createVuetify } from 'vuetify'
+import * as components from 'vuetify/components'
+const vuetify = createVuetify({
+    components,
+})
 const app = createApp(App)
 
 app.use(router)
-
+app.use(vuetify)
 app.mount('#app')
+
+
