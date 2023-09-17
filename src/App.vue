c1bca017a23311138922e67b1d1a2a1955aef25d
mamadjavaad <mamadjavad8237@gmail.com>
2023-09-17 20:43:06 +0330

    Setting theme to user's default theme☀️🌛

--- src/App.vue
+++ src/App.vue
@@ -1,13 +1,23 @@
 <script setup>
+//importing components
+import Navbar from './components/Navbar.vue'
+//setting theme to user's default theme
+import {onMounted} from 'vue'
+import { useTheme } from 'vuetify'
+const theme = useTheme()
+onMounted(() => {
+  if (window.matchMedia && window.matchMedia('(prefers-color-scheme: dark)').matches) {
+    theme.global.name.value = 'dark'
+  }
+})
 </script>
 
 <template>
-  <v-layout>
- 
-    
-    <routerView />
-    
-  </v-layout>
+  <v-layout class="d-flex flex-column flex-wrap">
+    <Navbar />
 
+    <routerView />
+
+  </v-layout>
 </template>
 
