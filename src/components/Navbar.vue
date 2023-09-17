06af9ba7067d5a51166a4b547729198e658ca440
mamadjavaad <mamadjavad8237@gmail.com>
2023-09-17 20:43:34 +0330

    Adding Navbar ✔️

---
+++ src/components/Navbar.vue
@@ -0,0 +1,34 @@
+<script setup>
+// toggle theme
+import { useTheme } from 'vuetify'
+const theme = useTheme()
+const toggleTheme = ()=>{
+  theme.global.name.value = theme.global.current.value.dark ? 'light' : 'dark'
+}
+</script>
+
+<template>
+<nav class="w-100 d-flex flex-column flex-wrap pa-5 border-b">
+
+  <div class="w-100  bg-transparent d-flex justify-space-between align-center ">
+    <a href="https://github.com/mamadjavaad" target="_blank">
+      <v-btn icon="mdi-github" variant="text"></v-btn>  
+    </a>
+    <span class="text-red-accent-2 text-h6">News App</span>
+    <v-btn icon="mdi-theme-light-dark"  @click="toggleTheme()"></v-btn>
+  </div>
+  <div class="w-100 d-flex justify-center align-center mt-5">
+    <v-responsive max-width="550" class="rounded-xl rounded">
+        <div>
+          <v-text-field  density="compact" variant="solo-filled"  label="Search In News"
+            append-inner-icon="mdi-magnify" single-line hide-details v-model="searchModel" @keydown.enter="searchSubmit"
+            @click:append-inner="searchSubmit"></v-text-field>
+        </div>
+
+      </v-responsive>
+  </div>
+</nav>
+ 
+</template>
+
+<style lang="scss"></style>
