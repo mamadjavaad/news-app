a28913fa080326a4d580793963e7d111c44f15c6
mamadjavaad <mamadjavad8237@gmail.com>
2023-09-23 17:18:38 +0330

    Using store data ⚡

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,29 +1,53 @@
 <script setup>
-import { ref } from 'vue'
+import { ref, onMounted, computed } from 'vue'
 // Defines SelctedCategory  
 const categoryValue = ref(0)
+//getting datas from store.js
+import useState from '../stores/data';
+const {newsData,category,getData} =useState()
 
+// filtering newsItems for showing everything except the first item
+const newsItems = computed(() => {
+  if (newsData) {
+    return newsData.value.filter((_, index) => index !== 0)
+  } else {
+    return null
+  }
+
+});
+//changing data based by category selection
+const changeData = (selected) => {
+  newsData.value = null;
+  category.value = selected
+  getData()
+}
+//running getData function from stor on Mounted
+onMounted(() => {
+  getData()
+})
 </script>
 
 <template>
   <v-main class="d-flex align-center flex-column h-100">
     <!-- for desktops and tvs -->
     <div class="w-100 d-none justify-center d-sm-flex d-lg-flex d-md-flex d-xl-flex d-xxl-flex">
-      <v-btn class="px-6 py-3 rounded-0 " variant="text">World</v-btn>
-      <v-btn class="px-6 py-3 rounded-0 " variant="text">Tech</v-btn>
-      <v-btn class="px-6 py-3 rounded-0 " variant="text">science</v-btn>
-      <v-btn class="px-6 py-3 rounded-0 " variant="text">Sports</v-btn>
-      <v-btn class="px-6 py-3 rounded-0 " variant="text">Health</v-btn>
+      <v-btn class="px-6 py-3 rounded-0 " variant="text" @click="changeData(null)">World</v-btn>
+      <v-btn class="px-6 py-3 rounded-0 " variant="text" @click="changeData('technology')">Tech</v-btn>
+      <v-btn class="px-6 py-3 rounded-0 " variant="text" @click="changeData('science')">science</v-btn>
+      <v-btn class="px-6 py-3 rounded-0 " variant="text" @click="changeData('sports')">Sports</v-btn>
+      <v-btn class="px-6 py-3 rounded-0 " variant="text" @click="changeData('politics')">Poltics</v-btn>
+
+  
     </div>
     <!--  -->
     <!-- for mobile and tablets -->
     <v-bottom-navigation class="d-flex d-sm-none d-lg-none d-md-none d-xl-none d-xxl-none" v-model="categoryValue"
       color="red-accent-1" grow>
       <v-btn>
-        <v-icon icon="mdi-earth"></v-icon>
+        <v-icon icon="mdi-earth" @click="changeData(null)"></v-icon>
         World
       </v-btn>
-      <v-btn>
+      <v-btn @click="changeData('technology')">
         <v-icon icon="mdi-text-recognition"></v-icon>
         Tech
       </v-btn>
@@ -33,101 +57,77 @@ const categoryValue = ref(0)
         <v-overlay activator="parent" location-strategy="static" class="more-category-xs">
           <v-list>
             <v-list-subheader>Other catagories</v-list-subheader>
-            <v-list-item prepend-icon="mdi-microscope" title="Science"></v-list-item>
-            <v-list-item prepend-icon="mdi-basketball" title="Sports"></v-list-item>
-            <v-list-item prepend-icon="mdi-hospital-box" title="Health"></v-list-item>
+            <v-list-item prepend-icon="mdi-microscope" title="Science" @click="changeData('science')"></v-list-item>
+            <v-list-item prepend-icon="mdi-basketball" title="Sports" @click="changeData('sports')"></v-list-item>
+            <v-list-item prepend-icon="mdi-bank-outline" title="Poltics" @click="changeData('politics')"></v-list-item>
           </v-list>
         </v-overlay>
       </v-btn>
     </v-bottom-navigation>
     <!--  -->
-    <div class="w-100 pa-4 mx-auto">
+    <div class="w-100 pa-4 mx-auto" v-if="newsData">
 
       <v-card class="mx-auto main-headline pa-5" variant="outlined">
 
         <v-card-title class="headline font-weight-bold text-center">
-          Fact check: Trump falsely claims Raffensperger said former president ‘didn't do anything wrong' on their
-          2021 phone call
+          {{ newsData[0].title }}
         </v-card-title>
         <v-card-actions>
-          <v-btn text>BBC</v-btn>
+          <v-btn text>{{ newsData[0].source_id }}</v-btn>
           <v-spacer></v-spacer>
-          <span class="caption grey--text">5 min ago</span>
+          <span class="caption grey--text">{{ newsData[0].pubDate }}</span>
         </v-card-actions>
 
-        <v-img
-          src="https://i.abcnewsfe.com/a/32fd20d9-0750-495d-af6a-81203537e23e/wirestory_839920116a244df3e55bdedf33820a80_16x9.jpg?w=992"
-          height="300px" cover></v-img>
+        <v-img :src="newsData[0].image_url" height="300px" cover></v-img>
 
         <v-card-subtitle class="grey--text">
-          Washington CNN — Georgia Secretary of State Brad Raffensperger has long been a pointed critic of former
-          President Donald Trump's conduct on a January 2, 2021, phone call in which Trump told numerous lies about
-          supposed election fraud and pressured Raffensperger to somehow 'find' enough votes to overturn his defeat in
-          Georgia in the 2020 election. In an interview that aired Sunday on NBC's 'Meet the Press,' though, Trump
-          claimed Raffensperger had recently declared Trump's conduct on the call perfectly acceptable.
+          {{ newsData[0].description }}
+
         </v-card-subtitle>
         <v-card-actions>
-          <v-btn class="mx-auto">Article Link</v-btn>
-
+          <a :href="newsData[0].link" target="_blank" class="mx-auto">
+            <v-btn>Article Link</v-btn>
+          </a>
         </v-card-actions>
 
       </v-card>
 
     </div>
 
-    <div class="news-container d-flex flex-wrap justify-center ">
-      <div class="news-item mx-6 my-4">
+    <div class="news-container d-flex flex-wrap justify-center " v-if="newsData">
+      <div class="news-item mx-6 my-4" v-for="news in newsItems" :key="news">
         <v-card class="pa-5" min-width="200px" variant="outlined">
           <v-card-title class="headline font-weight-bold">
-            Example News Headline
+            {{ news.title }}
           </v-card-title>
           <v-card-actions>
-            <v-btn text>cnn</v-btn>
+            <v-btn text>{{ news.source_id }}</v-btn>
             <v-spacer></v-spacer>
-            <span class="caption grey--text">5 min ago</span>
+            <span class="caption grey--text">{{ news.pubDate }}</span>
           </v-card-actions>
 
-          <v-img
-            src="https://i.abcnewsfe.com/a/32fd20d9-0750-495d-af6a-81203537e23e/wirestory_839920116a244df3e55bdedf33820a80_16x9.jpg?w=992"
-            height="300px" cover></v-img>
+          <v-img :src="news.image_url" height="300px" cover></v-img>
 
           <v-card-subtitle class="grey--text">
-            This is where the news summary could go. This is where the news summary could go.
+            {{ news.description }}
           </v-card-subtitle>
 
           <v-card-actions>
-            <v-btn class="mx-auto">Article Link</v-btn>
+            <a :href="news.link" target="_blank">
+              <v-btn class="mx-auto">Article Link</v-btn>
+            </a>
 
           </v-card-actions>
         </v-card>
       </div>
-      <div class="news-item mx-6  my-4">
-        <v-card class="pa-5" min-width="200px" variant="outlined">
-          <v-card-title class="headline font-weight-bold">
-            Example News Headline
-          </v-card-title>
-          <v-card-actions>
-            <v-btn text>cnn</v-btn>
-            <v-spacer></v-spacer>
-            <span class="caption grey--text">5 min ago</span>
-          </v-card-actions>
 
-          <v-img
-            src="https://i.abcnewsfe.com/a/32fd20d9-0750-495d-af6a-81203537e23e/wirestory_839920116a244df3e55bdedf33820a80_16x9.jpg?w=992"
-            height="300px" cover></v-img>
-
-          <v-card-subtitle class="grey--text">
-            This is where the news summary could go. This is where the news summary could go.
-          </v-card-subtitle>
-
-          <v-card-actions>
-            <v-btn class="mx-auto">Article Link</v-btn>
-
-          </v-card-actions>
-        </v-card>
-      </div>
     </div>
+    <v-progress-linear indeterminate v-if="!newsData" class="d-flex d-sm-none d-lg-none d-md-none d-xl-none d-xxl-none"></v-progress-linear>
 
+    <div v-if="!newsData" class="mt-8">
+      <v-progress-circular indeterminate color="white" :size="50"></v-progress-circular>
+
+    </div>
 
   </v-main>
 </template>
@@ -159,7 +159,6 @@ const categoryValue = ref(0)
   }
 
   .news-container {
-    width: 93%;
     max-width: none !important;
     min-width: 570px !important;
 
@@ -172,13 +171,20 @@ const categoryValue = ref(0)
 
 }
 
-.v-card-title,
-.v-card-subtitle {
+.v-card-title {
   white-space: pre-wrap !important;
   padding: 10px;
 }
 
-.news-item {
-  width: 94%;
+.v-card-subtitle {
+  padding: 10px;
+}
+
+.news-container {
+  width: 93%;
+
+  .news-item {
+    width: 100% ;
+  }
 }
 </style>
